2abd7b400b985c62cbf6bb0ad8567c713756b85b
José Fernandes <jose-vitor-7@hotmail.com>
2020-11-17 19:39:02 -0300

    Bacana V1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  photoURL:string;
 
-  constructor() {}
+  cameraOptions: CameraOptions = {
+    quality: 100, // qualidade
+    destinationType: this.camera.DestinationType.DATA_URL, // destino
+    encodingType: this.camera.EncodingType.JPEG, // codificação
+    mediaType: this.camera.MediaType.PICTURE, // tipo de mídia
+  }
 
+  constructor(private camera: Camera) {}
+
+  takePhoto(){
+    this.camera.getPicture(this.cameraOptions).then(imageData => {
+      const base64Image = `data:image/jpeg;base64,${imageData}`;
+      this.photoURL = base64Image;
+    }, err => console.log(err));
+  }
 }
